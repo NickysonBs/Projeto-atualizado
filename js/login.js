c96929af5d9ddb11a7bbc769ca914fa25994e4ff
Nickysonbs <nathalya.batista@academico.ifpb.edu.br>
2023-01-13 16:14:36 -0300

    alt

---
+++ js/login.js
@@ -0,0 +1,38 @@
+function login(){
+    const username = "gabriel";
+    const password = "senha123";
+
+    let usuario_informado = document.getElementById("username").value;
+
+    let senha_informada = document.getElementById("password").value;
+
+   if(usuario_informado == username
+           && senha_informada == password){
+
+        let continuar_logado = document.getElementById("remember-me");
+
+        if(continuar_logado){
+            document.cookie = "logado=1";
+        }
+
+        window.location.href = "paginainicial.html";
+    } else {
+        alert("Login ou senha invalidos");
+    }
+
+}
+
+function register(){
+    let username = document.getElementById("username").value;
+    
+    let email = document.getElementById("email").value;
+    
+    let password = document.getElementById("password").value;
+    
+    let repeat_password = document.getElementById("repeat-password").value;
+
+    if(password != repeat_password ){
+        alert("Você deve digitar duas senhas iguais.");
+    }
+    
+}
